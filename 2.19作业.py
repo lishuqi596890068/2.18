50e6ae529cb349f49d27dcbb4099298856f2d1a6
59689 <596890068@qq.com>
2019-02-20 21:09:13 +0800

    aa

---
+++ 2.19作业.py
@@ -0,0 +1,22 @@
+import random
+# for i in range(1,10):
+#     for j in range(1,i+1):
+#         d = i*j
+#         print('%d*%d=%-2d'%(i,j,d),end = ' ')
+#     print()
+
+# def nei(n):
+#     for j in range(1,n+1):
+#         d = n*j
+#         print('%d*%d=%-2d'%(n,j,d),end = ' ')
+#
+# def wai():
+#     for i in range(1,10):
+#         nei(i)
+#         print()
+# wai()
+tup1 = (12,34.56)
+tup2 = ('abc','xyz')
+tup3 = tup1 + tup2
+print(tup3[1])
+
